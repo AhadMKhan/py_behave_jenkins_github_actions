4d7f254fc464b5911e42a03f1d0e6d2cc0b0f145
Ahad Muhammad Khan <ahad.mohammad.khan@gmail.com>
2024-05-25 16:45:06 +0500

    test runners for pipeline

---
+++ test_runners/test_runner_pipeline.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Call the run_test.bat script
+call pre_test_runner_pipeline.bat %*
+
+REM Check if index.html exists before renaming
+if exist "test-results\allure-report\index.html" (
+    ren "test-results\allure-report\index.html" "Automation Test Execution Report.html"
+)
+
+REM Prompt user to press any key before exiting
+cd test_runners
+echo Allure report generation completed. Press any key to exit.
